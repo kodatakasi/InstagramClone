e500c378144b888d5371749aa1c992d7e0f60caa
koda takasi <kodatakasi@gmail.com>
2019-09-12 17:27:17 +0900

    session機能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def authenticate_user
+    if session[:user_id] == nil
+    redirect_to new_session_path
+    end
+  end
 end
