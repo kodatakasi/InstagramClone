161555c3ad14774746f994d9885dfd09c1a272c0
koda takasi <kodatakasi@gmail.com>
2019-09-17 15:39:15 +0900

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do |n|
+  name = Faker::JapaneseMedia::DragonBall.character 
+  email = Faker::Internet.email
+  password = "password"
+  profile = Faker::Lorem.word
+  profile_image = open("/Users/kodatakasi/workspace/rails/InstagramClone/public/assets/blank-profile-picture-973460_640.png")
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               profile: profile,
+               profile_image: profile_image,
+               )
+end
