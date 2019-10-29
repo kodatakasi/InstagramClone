a3cc0935dfd3f162ea8df7b442c6e0787ecbc481
koda takasi <kodatakasi@gmail.com>
2019-10-29 14:47:55 +0900

    サインイン挙動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   mount_uploader :profile_image, ImageUploader
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 225 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: true
   before_validation { email.downcase! }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
